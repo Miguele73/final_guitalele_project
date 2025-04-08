c34cdb8bec73b68c5772b476fc63eb24d95ad33c
Miguele73 <cabusam.eu@gmail.com>
2025-04-08 16:02:24 +0200

    DesitionScreengebaut

---
+++ lib/desiction_screen.dart
@@ -0,0 +1,75 @@
+import 'package:final_guitalele_project/desicions_box.dart';
+import 'package:final_guitalele_project/one_back_button.dart';
+import 'package:flutter/material.dart';
+
+class DesictionScreen extends StatelessWidget {
+  const DesictionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF505160),
+      appBar: AppBar(
+        toolbarHeight: 250,
+        backgroundColor: const Color(0xFF505160),
+        title: Image.asset('assets/images/logo.png', height: 200, width: 200),
+
+        // flexibleSpace: Padding(
+        // padding: const EdgeInsets.all(32),
+        // child: SizedBox(
+        // height: 200,
+        // width: 200,
+        // child:
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      floatingActionButton: const OneBackButton(),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(48, 0, 16, 128),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DesicionsBox(
+              color: Color(0xFFDE7A22),
+              selectionText: 'Songs',
+              imageIcon: ImageIcon(
+                AssetImage('assets/iconImages/music-4SongsIcon.png'),
+              ),
+            ),
+            DesicionsBox(
+              color: Color(0xFFD5C9B1),
+              selectionText: 'Chords',
+              imageIcon: ImageIcon(
+                AssetImage('assets/iconImages/ChordIcon.png'),
+              ),
+            ),
+            DesicionsBox(
+              color: Color(0xFFBCBABE),
+              selectionText: 'Scales',
+              imageIcon: ImageIcon(
+                AssetImage('assets/iconImages/ClefIcon.png'),
+                size: 28,
+              ),
+            ),
+            DesicionsBox(
+              color: Color(0xFFBFDCCF),
+              selectionText: 'Chordliste',
+              imageIcon: ImageIcon(
+                AssetImage('assets/iconImages/ChordIcon.png'),
+              ),
+            ),
+            DesicionsBox(
+              color: Color(0xFFE05858),
+              selectionText: 'Meine Favoriten',
+              imageIcon: ImageIcon(
+                AssetImage('assets/iconImages/FavoritesBookmark.png'),
+                size: 24,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:final_guitalele_project/desicions_box.dart';
-import 'package:final_guitalele_project/one_back_button.dart';
+import 'package:final_guitalele_project/desiction_screen.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,70 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      home: Scaffold(
-        backgroundColor: const Color(0xFF505160),
-        appBar: AppBar(
-          toolbarHeight: 250,
-          backgroundColor: const Color(0xFF505160),
-          title: Image.asset('assets/images/logo.png', height: 200, width: 200),
-
-          // flexibleSpace: Padding(
-          // padding: const EdgeInsets.all(32),
-          // child: SizedBox(
-          // height: 200,
-          // width: 200,
-          // child:
-        ),
-
-        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-        floatingActionButton: const OneBackButton(),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(48, 0, 16, 128),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              DesicionsBox(
-                color: Color(0xFFDE7A22),
-                selectionText: 'Songs',
-                imageIcon: ImageIcon(
-                  AssetImage('assets/iconImages/music-4SongsIcon.png'),
-                ),
-              ),
-              DesicionsBox(
-                color: Color(0xFFD5C9B1),
-                selectionText: 'Chords',
-                imageIcon: ImageIcon(
-                  AssetImage('assets/iconImages/ChordIcon.png'),
-                ),
-              ),
-              DesicionsBox(
-                color: Color(0xFFBCBABE),
-                selectionText: 'Scales',
-                imageIcon: ImageIcon(
-                  AssetImage('assets/iconImages/ClefIcon.png'),
-                  size: 28,
-                ),
-              ),
-              DesicionsBox(
-                color: Color(0xFFBFDCCF),
-                selectionText: 'Chordliste',
-                imageIcon: ImageIcon(
-                  AssetImage('assets/iconImages/ChordIcon.png'),
-                ),
-              ),
-              DesicionsBox(
-                color: Color(0xFFE05858),
-                selectionText: 'Meine Favoriten',
-                imageIcon: ImageIcon(
-                  AssetImage('assets/iconImages/FavoritesBookmark.png'),
-                  size: 24,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: DesictionScreen(),
     );
   }
 }
